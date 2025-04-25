6cffde6eb1cb44f3e27df30bf946bd3448c021db
ilukssa <ilya.sakantsev@mail.ru>
2025-04-25 21:31:17 +0300

    add dump and load data funcs

--- main.py
+++ main.py
@@ -1,3 +1,14 @@
+import json
+import datetime
+
+notes_path = "entries/notes.json"
+
+def load_data_from_json(file_path):
+    pass
+
+def dump_data_to_json(file_path, new_data):
+    pass
+
 def main():
     pass
 
