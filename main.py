6fb6f5d1d7100c679e6a3cb37ef1347503144848
ilukssa <ilya.sakantsev@mail.ru>
2025-04-25 21:36:05 +0300

    ref load and dump func

--- main.py
+++ main.py
@@ -4,13 +4,16 @@ import datetime
 notes_path = "entries/notes.json"
 
 def load_data_from_json(file_path):
-    pass
+    with open(file_path, "r") as f:
+        data = json.load(f)
+    return data
 
 def dump_data_to_json(file_path, new_data):
-    pass
+    with open(file_path, "w") as f:
+        json.dump(new_data, f)
 
 def main():
-    pass
+    
 
 if __name__ == "__main__":
     main()
