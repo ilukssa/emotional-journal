952b5fd24efdc3aee08415fc6543f1dd20808fd2
ilukssa <ilya.sakantsev@mail.ru>
2025-04-25 22:22:05 +0300

    except JSONDecodeError

--- main.py
+++ main.py
@@ -4,9 +4,12 @@ import datetime
 notes_path = "entries/notes.json"
 
 def load_data_from_json(file_path):
-    with open(file_path, "r") as f:
-        data = json.load(f)
-    return data
+    try:
+        with open(file_path, "r") as f:
+            data = json.load(f)
+        return data
+    except(json.JSONDecodeError):
+        return {}
 
 def dump_data_to_json(file_path, new_data):
     with open(file_path, "w") as f:
